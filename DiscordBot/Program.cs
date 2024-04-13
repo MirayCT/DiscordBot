541a196292ba15228b42bece58995b44c39d4f4b
MirayCT <mctuncel.141999@gmail.com>
2024-04-13 14:40:33 +0300

    Add project files.

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    internal class Program
+    {
+        //make an instance of a discord client
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static void Main(string[] args)
+        { //program the bot configuration and start up the bot
+           
+            //Setting a prefix so the token is not exposed in config.json file
+
+        }
+    }
+}
---
+++ DiscordBot/config/JSONReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.config
+{
+    internal class JSONReader
+    {
+        public string token {  get; set; }  
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using(StreamReader sr = new StreamReader("config.json")) 
+            { 
+
+            }
+        }
+    }
+
+    internal sealed class JSONStructor
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
